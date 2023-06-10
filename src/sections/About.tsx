d8923d867b76ffd0c056182bec458394e1f6538a
Karan Singh <karanrsingh2662006@gmail.com>
2023-06-10 16:17:33 +0530

    fixed a typo

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -12,21 +12,25 @@ const About = (props: Props) => {
     <section id="about" className="section flex flex-col ">
       <SectionTitle>Who am i?</SectionTitle>
 
-      <motion.div initial="hidden"
+      <motion.div
+        initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.5 }}
-        
-        className="flex flex-col max-md:gap-4 md:flex-row max-md:items-center justify-around ">
+        className="flex flex-col max-md:gap-4 md:flex-row max-md:items-center justify-around "
+      >
         <div className="relative ">
-          <motion.span  variants={zoomIn(0, 1)} className="absolute -left-20 -top-20 bg-gradient-radial md:h-[500px] md:w-[430px]"></motion.span>
+          <motion.span
+            variants={zoomIn(0, 1)}
+            className="absolute -left-20 -top-20 bg-gradient-radial md:h-[500px] md:w-[430px]"
+          ></motion.span>
 
-  {/* My Images  */}
+          {/* My Images  */}
           <div className="relative   max-md:mx-auto w-3/4">
             {/* Sketch image */}
             <motion.div
               initial="initial"
-              whileHover={'drop'}
-              whileInView='animate'
+              whileHover={"drop"}
+              whileInView="animate"
               variants={wallDrop}
               className="absolute   max-md:top-[-9.5px] top-[-11px] "
             >
@@ -36,7 +40,6 @@ const About = (props: Props) => {
                 height={453}
                 width={377.93}
               />
-             
             </motion.div>
 
             {/* Colorfull Image */}
@@ -50,14 +53,37 @@ const About = (props: Props) => {
           </div>
         </div>
         <motion.div
-
-        variants={slideIn('left')}  
-        className="flex justify-center items-start gap-2 flex-col">
-          <motion.span variants={fadeIn("left", "spring",0* 0.5)} className="text-2xl">I am a</motion.span>
-          <motion.div variants={fadeIn("left", "spring",0.1* 0.5)} className="text-4xl">Self Thaught</motion.div>
-          <motion.div variants={fadeIn("left", "spring",0.2* 0.5)} className="text-4xl">Web Developer</motion.div>
-          <motion.span variants={fadeIn("left", "spring",0.3* 0.5)} className="text-2xl">coding since the age of</motion.span>
-          <motion.span variants={fadeIn("left", "spring",0.4* 0.5)} className=" block text-4xl md:cursor-pointer  text-secondary">
+          variants={slideIn("left")}
+          className="flex justify-center items-start gap-2 flex-col"
+        >
+          <motion.span
+            variants={fadeIn("left", "spring", 0 * 0.5)}
+            className="text-2xl"
+          >
+            I am a
+          </motion.span>
+          <motion.div
+            variants={fadeIn("left", "spring", 0.1 * 0.5)}
+            className="text-4xl"
+          >
+            Self Taught
+          </motion.div>
+          <motion.div
+            variants={fadeIn("left", "spring", 0.2 * 0.5)}
+            className="text-4xl"
+          >
+            Web Developer
+          </motion.div>
+          <motion.span
+            variants={fadeIn("left", "spring", 0.3 * 0.5)}
+            className="text-2xl"
+          >
+            coding since the age of
+          </motion.span>
+          <motion.span
+            variants={fadeIn("left", "spring", 0.4 * 0.5)}
+            className=" block text-4xl md:cursor-pointer  text-secondary"
+          >
             fourteen
           </motion.span>
           {/* <span className="invisible hover:visible md:cursor-pointer " >14</span> */}
