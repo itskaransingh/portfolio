af0fdc6a7af40134a238158b5bc6743ffb0d9b1a
Karan Singh <karanrsingh2662006@gmail.com>
2023-09-07 18:20:39 +0530

    some changes in og image

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -17,9 +17,9 @@ const Navbar = (props: Props) => {
     whileInView="show"
     className={jakarta.className + 'relative lower-glassmorphism bg-nav  h-16 shadow-md lg:px-[132px] md:px-[80px] px-[23px] py-3 w-full'}>
         <div className="flex justify-between  items-center">
-          <div>
+          <Link href={'/'}>
             <Image src='/svg/logo.svg'  alt="my logo" height={28} width={28}/>
-          </div>
+          </Link>
           <div className="flex  max-lg:fixed     bottom-3 max-lg:rounded-md  left-0 right-0 md:text-base max-lg:text-sm  max-lg:max-w-[90%] max-lg:mx-auto   font-semibold ">
             <div className=" flex max-lg:items-center h-full w-full  max-lg:relative max-lg:lower-glassmorphism max-lg:bg-secondary gap-[48px] max-md:gap-4 max-lg:justify-center max-lg:gap-8 max-lg:py-3 max-lg:px-2">
 
