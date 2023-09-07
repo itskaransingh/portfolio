334874488f71813e43df7cc6a33aa6e2a44d3ef1
Karan Singh <karanrsingh2662006@gmail.com>
2023-09-07 18:00:29 +0530

    seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,65 @@
 import { preahvihear } from './fonts'
-import { Footer, Navbar, SideSocial,SideNav } from '../components/UI'
+import { Footer, Navbar, SideSocial, SideNav } from '../components/UI'
 import './globals.css'
 import { AnalyticsWrapper } from '../components/analytics'
+import { Metadata } from 'next'
+import { siteConfig } from '../utils/siteConfig'
 
+export const metadata: Metadata = {
+  title: siteConfig.title,
+  description: siteConfig.description,
+  metadataBase: new URL(siteConfig.siteUrl),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  keywords: ["karan singh", "thekaransingh", "portfolio", "fullstack developer", "itskaransingh"],
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#170A28" },
+    { media: "(prefers-color-scheme: dark)", color: "#170A28" },
+  ],
+  icons: {
+    icon: siteConfig.logo,
+    shortcut: siteConfig.logo,
+    apple: siteConfig.logo,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.title,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+  },
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.siteUrl,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: siteConfig.title,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 800,
+        height: 600,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
+}
 
 
 export default function RootLayout({
@@ -12,11 +69,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={preahvihear.className}>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
       <body className='bg-primary scroll-smooth text-[#ffffff]'>
         <header className='fixed w-screen z-30 '>
           <Navbar />
@@ -25,11 +77,11 @@ export default function RootLayout({
           <SideNav />
         </aside>
         <main>
-        {children}
+          {children}
 
         </main>
         <aside>
-        <SideSocial />
+          <SideSocial />
         </aside>
         <footer className='flex justify-center items-center h-20'>
           <Footer />
---
+++ src/utils/siteConfig.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+    title: 'Karan Singh - A Full Stack Developer',
+    description: 'Hi my name is Karan Singh a 16 year old boy coding since the age of 14',
+    logo:"/svg/logo.svg",
+    siteUrl: 'https://itskaransingh.vercel.app/',
+    ogImage: './og.png',
+    
+}
